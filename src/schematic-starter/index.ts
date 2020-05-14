36552ab27e38b119cfcbbccf0b8365aae16e8707
-- <-->
2020-05-14 07:43:32 -0400

    Upd - index.ts

--- src/schematic-starter/index.ts
+++ src/schematic-starter/index.ts
@@ -13,7 +13,7 @@ export function schematicStarter(_options: any): Rule {
   return (tree: Tree, context: SchematicContext) => {
     const rules: Rule[] = [
       template({ ...strings, ..._options }),
-      move("sandbox/src/components")
+      move("src/app/components")
     ];
 
     // todo: use the url() function to retrieve the source from the './files' directory
