721e1f2deb54768f6cf72b13d6504e2752cc319e
-- <-->
2020-05-14 07:37:00 -0400

    Upd - Schematic Move

--- src/schematic-starter/index.ts
+++ src/schematic-starter/index.ts
@@ -3,24 +3,23 @@ import {
   SchematicContext,
   Tree,
   apply,
-  url
+  url,
+  template,
+  move
 } from "@angular-devkit/schematics";
+import { strings } from "@angular-devkit/core";
 
 export function schematicStarter(_options: any): Rule {
   return (tree: Tree, context: SchematicContext) => {
-    const rules: Rule[] = [];
+    const rules: Rule[] = [
+      template({ ...strings, ..._options }),
+      move("sandbox/src/components")
+    ];
 
     // todo: use the url() function to retrieve the source from the './files' directory
     const source = url("./files");
 
     // todo: use the apply() function to apply rules (empty for now) and invoke it with the context
     return apply(source, rules)(context);
-
-    //  Context Testing
-    // context.logger.debug("debug");
-    // context.logger.info("info");
-    // context.logger.warn("warn");
-    // context.logger.error("error");
-    // return tree;
   };
 }
